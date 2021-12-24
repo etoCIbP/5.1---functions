6e1113be5f5cc939e178f03425d47c3e086c636f
Teya <Teya@TVOROG>
2021-12-24 03:55:39 +0200

    Добавьте файлы проекта.

---
+++ 5.1 - functions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _5._1___functions
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+				float SqrtK(float x, int k, int n)
+				{
+					if ((n == 0) || (x == 1)) return 1;
+					if (x == 0) return 0;
+					if (k == 1) return x;
+					else return ((float)(SqrtK(x, k, n - 1) - (SqrtK(x, k, n - 1) - (x / Math.Pow((SqrtK(x, k, n - 1)), (k - 1)))) / k));
+				}
+
+			double O;   //Ответ
+			int k, n;	//Переменные, целые
+			float x, y; //вещ. параметр
+			Console.WriteLine("Введите число x = ");
+			x = (float)double.Parse(Console.ReadLine());	//вводим переменные
+			Console.WriteLine("Введите число k = ");
+			k = (int)double.Parse(Console.ReadLine());
+			Console.WriteLine("Введите число n = ");
+			n = (int)double.Parse(Console.ReadLine());		//до сюда
+			O = SqrtK(x, k, n);								//ответ
+            Console.WriteLine("Ответ = " + O);
+
+		}
+	}
+}
